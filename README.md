9da336c9ee2c36a2f60680d4036c17ea5329de2a
Push Chen <littlepush+github@gmail.com>
2014-06-20 15:56:59 +0800

    parse the parameter finish

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ cleandns
 ========
 
 This is a DNS proxy client/server, it will route the UDP DNS request to local or remote parent DNS server according to the querying domain via TCP request.
+
+Install
+========
+    make release
+
+Filter File Format
+========
+    *.*domain*.*.*component*
+We can put a '*' in any part of the domain.
+
+Usage
+========
+Default server port is 11025
+* Client
+
+    `cleandns --client --filter <file> --server <server> --port <port> --local <dns>`
+
+* Server
+
+    `cleandns --server --port <port> --local <dns>`
+
+Example
+========
+
+    cleandns --client --filter ./filter.list --server <your_server_ip> --local 202.96.209.133
+    cleandns --server --local 8.8.8.8
+
+Version Log
+========
+v0.1    first version, rewrite. prototype is plib/pdns
--- src/main.cpp
+++ src/main.cpp
@@ -44,14 +44,129 @@
 #include "dns.h"
 
 void _cleandns_version_info() {
-
+    printf( "cleandns version: %s\n", VERSION );
+    printf( "target: %s\n", TARGET );
+    printf( "Visit <https://github.com/littlepush/cleandns> for more infomation.\n" );
 }
 
 void _cleandns_help_info() {
-
+    _cleandns_version_info();
+    printf( "cleandns --client --filter <file> --server <server> --port <port> --local <dns>\n");
+    printf( "cleandns --server --port <port> --local <dns>\n");
+    printf( "options: \n" );
+    printf( "    --filter|-f        The filter file path\n" );
+    printf( "    --server|-s        Server side address for proxy\n" );
+    printf( "    --port|-p          Server side port for proxy\n" );
+    printf( "    --local|-l         Local parent dns address\n" );
 }
 
 int main( int argc, char *argv[] ) {
+    const char *_home_path = getenv("HOME");
+    string _filter_list_file = string(_home_path) + "/.cleandns.filter";
+    string _server_address = "127.0.0.1";
+    unsigned int _server_port = 11025;
+    string _local_address = "202.96.209.133";
+    bool _is_client = false;
+    bool _is_server = false;
+
+    if ( argc >= 2 ) {
+        int _arg = 1;
+        for ( ; _arg < argc; ++_arg ) {
+            string _command = argv[_arg];
+            if ( _command == "-h" || _command == "--help" ) {
+                // Help
+                _cleandns_help_info();
+                return 0;
+            }
+            if ( _command == "-v" || _command == "--version" ) {
+                // Version
+                _cleandns_version_info();
+                return 0;
+            }
+            if ( _command == "-f" || _command == "--filter" ) {
+                if ( _arg + 1 < argc ) {
+                    _filter_list_file = argv[++_arg];
+                } else {
+                    cerr << "Invalidate argument: " << _command << ", missing parameter." << endl;
+                    return 1;
+                }
+                continue;
+            }
+            if ( _command == "-l" || _command == "--local" ) {
+                if ( _arg + 1 < argc ) {
+                    _local_address = argv[++_arg];
+                } else {
+                    cerr << "Invalidate argument: " << _command << ", missing parameter." << endl;
+                    return 1;
+                }
+                continue;
+            }
+            if ( _command == "-s" || _command == "--server" ) {
+                if ( _arg + 1 < argc ) {
+                    _server_address = argv[++_arg];
+                } else {
+                    cerr << "Invalidate argument: " << _command << ", missing parameter." << endl;
+                    return 1;
+                }
+                continue;
+            }
+            if ( _command == "-p" || _command == "--port" ) {
+                if ( _arg + 1 < argc ) {
+                    string _port = argv[++_arg];
+                    _server_port = atoi(_port.c_str());
+                    if ( _server_port == 0 || _server_port > 65535 ) {
+                        cerr << "Invalidate port: " << _server_port << "." << endl;
+                        return 1;
+                    } 
+                } else {
+                    cerr << "Invalidate argument: " << _command << ", missing parameter." << endl;
+                    return 1;
+                }
+                continue;
+            }
+            if ( _command == "--client" ) {
+                _is_client = true;
+                continue;
+            }
+            if ( _command == "--server" ) {
+                _is_server == true;
+            }
+            cerr << "Invalidate argument: " << _command << "." << endl;
+            return 1;
+        }
+    }
+
+    if ( _is_client && _is_server ) {
+        cerr << "Cannot be server and client at same time." << endl;
+        return 1;
+    }
+    if ( !(_is_client || _is_server) ) {
+        cerr << "Must specified to be client or server." << endl;
+        return 1;
+    }
+
+    pid_t _pid = fork();
+    if ( _pid < 0 ) {
+        cerr << "Failed to create child process." << endl;
+        return 2;
+    }
+    if ( _pid > 0 ) {
+        // Has create the child process.
+        return 0;
+    }
+
+    if ( setsid() < 0 ) {
+        cerr << "failed to set session leader for child process." << endl;
+        return 3;
+    }
+
+    cout << "Filter File: " << _filter_list_file << endl;
+    cout << "Server Port: " << _server_port << endl;
+    cout << "Server Address: " << _server_address << endl;
+    cout << "Local Address: " << _local_address << endl;
+    cout << "Is Client: " << _is_client << endl;
+    cout << "is Server: " << _is_server << endl;
+
     return 0;
 }
 
