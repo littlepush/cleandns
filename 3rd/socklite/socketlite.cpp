e9bbd6c721838d241331cd699979c393ecb96bc3
Push Chen <littlepush+github@gmail.com>
2015-11-19 18:18:56 +0800

    update udp socket

--- 3rd/socklite/socketlite.cpp
+++ 3rd/socklite/socketlite.cpp
@@ -881,6 +881,13 @@ bool sl_udpsocket::connect( const string &ipaddr, uint32_t port, uint32_t timeou
     if ( SOCKET_NOT_VALIDATE(m_socket) ) {
         return false;
     }
+    // Bind to 0, so we can get the port number by getsockname
+    struct sockaddr_in _usin = {};
+    _usin.sin_family = AF_INET;
+    _usin.sin_addr.s_addr = htonl(INADDR_ANY);
+    _usin.sin_port = 0;
+    bind(m_socket, (struct sockaddr *)&_usin, sizeof(_usin));
+
     return true;
 }
 // Listen on specified port and address, default is 0.0.0.0
